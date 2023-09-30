e247bc4fba0a2360b2a907f7de93148a1b1f6224
ahmet keser <ahmetkeser006@gmail.com>
2023-10-01 02:30:16 +0300

    verituru.js eklendi

--- jscript/004-boolean-veriler.js
+++ jscript/004-boolean-veriler.js
@@ -6,9 +6,29 @@ console.log(isActive)
     tunnel.appendChild(addElement)                  // ekleme komutu ile paketi gönder
 isActive = false    // sonradan değer değiştirilebilir.
 console.log(isActive)
-
+// --------------Var Yok Kontrolü--------
+//--------------- 0(false)    1(true) --------------
 let userName;
 let isUserName =Boolean(userName) // userName değişkeninin boolen değerinin atamasını yapar
 console.log(isUserName)            // içi null olduğu için false değeri döndürür
-Boolean("11")           //direkt atama yapılabilir. false değeri verir
+ //direkt atama yapılabilir. false değeri verir
 // bu bizim işimize değerlerin girilip girilmediği yada çekilip çekilmediği kontrolünde işimize yarar.
+
+userName ="user"
+console.log("User Name var mı:",Boolean(userName))
+Boolean(9<4)  // false
+Boolean("11") // true
+Boolean("0") // true
+Boolean("") // false
+
+userName = "user"
+
+// 0 ,-0, null, false, NaN, undefined, ("") ->
+Boolean(0) //  false
+Boolean(-0) // false
+Boolean(-0.1) // true
+Boolean(0 === 0) // true
+
+// Not: Karar Yapilari Kisminda Tekrar Anlatilacak
+Boolean(userName.length > 0) // true
+
