cedfde31b83b60f0de34e7b534e0491bd656c64a
ahmet keser <ahmetkeser006@gmail.com>
2023-10-02 00:27:23 +0300

    promtp-listeelemanekleme-domislemleri

---
+++ jscript/009-prompt.js
@@ -0,0 +1,6 @@
+// ********** prompt ile Kullanicidan Bilgi Almak **********
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+
+let fullName = prompt("Lutfen Adinizi Giriniz: ")
+let greeting = document.querySelector("#promptIndex")
+greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
