734f5c234369d6350fc7e37a91d7b203ba087895
ahmet keser <ahmetkeser006@gmail.com>
2023-10-05 23:33:59 +0300

    local stript ekleme

--- jscript/019-localscript-deneme.js
+++ jscript/019-localscript-deneme.js
@@ -1,4 +1,6 @@
-let counter =0
+//localde saklanan değer varsa bunu numbere çevirerek getirir
+// yoksa 0 değerini alır.
+let counter =localStorage.getItem('counter') ? Number(localStorage.getItem('counter')): 0
 let counterDOM =document.querySelector("#counter")
 let positiveBtn=document.querySelector("#positiveBtn")
 let negativeBtn=document.querySelector("#negativeBtn")
@@ -9,15 +11,23 @@ negativeBtn.addEventListener("click",clickEvent)
 function clickEvent (){
     console.log(typeof(counter))
     console.log(this.id)
-    if(this.id == "positiveBtn"){
-        counterDOM.innerHTML = counter += 1
-    }else{
-        counterDOM.innerHTML = counter -= 1
-    }
-   // this.id =="positiveBtn" ? counter += 1 :counter -=1  // Kısa Hali
-   //     counterDOM.innerHTML = counter
+    this.id =="positiveBtn" ? counter += 1 :counter -=1  // Kısa Hali
+    localStorage.setItem("counter",counter) // değeri localde saklar
+    counterDOM.innerHTML = counter
+    
+    // bu if yapısı yukarda fat arrow ile kısa yapılmıştır
+    // if(this.id == "positiveBtn"){
+    //     counterDOM.innerHTML = counter += 1
+    // }else{
+    //     counterDOM.innerHTML = counter -= 1
+    // }
 }
 
-let ingClass =["abouth","above","across","action","activity","actor"]
-let trClass =["hakkında","üzerine","karşıya","çalışma","etkinlik","aktör"]
-localStorage.setItem("abouth","hakkında")
+
+
+
+// for döngüsü ile dizileri key value eşleştir ve butonlara tanımlayarak 
+// bir çalışma yapılacak
+//    let ingClass =["abouth","above","across","action","activity","actor"]
+//  let trClass =["hakkında","üzerine","karşıya","çalışma","etkinlik","aktör"]
+//    localStorage.setItem("abouth","hakkında")
