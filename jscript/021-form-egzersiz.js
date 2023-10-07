efae438d1242d7f461e808cc55edd9c2fa180700
ahmet keser <ahmetkeser006@gmail.com>
2023-10-07 16:30:06 +0300

    form egzersiz eklendi

--- jscript/021-form-egzersiz.js
+++ jscript/021-form-egzersiz.js
@@ -1,33 +1,38 @@
 let userFormDOM = document.querySelector('#userForm')
 userFormDOM.addEventListener('submit',formHandler)
+const alertDOM = document.querySelector('#alert')
+
+const alertFunction = (message,clasName="")=>`
+<div class="alert alert-${clasName} alert-dismissible fade show" role="alert">
+<strong>Hey!</strong> ${message}
+<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
 
 function formHandler(event){
-    
-    event.PreentDefault()
-    const USER_NAME = document.querySelector('#username')
+    event.preventDefault() // default işlemi engelledi sayfa göndere bastığında adres çubuğunda parametre çıkmayacak
+    // let scoreInputDOM = document.querySelector('#score')
+    // localStorage.setItem('score',scoreInputDOM.value)
+    const USERNAME = document.querySelector('#userName')
     const SCORE = document.querySelector('#score')
-    AddItem(USER_NAME.value, score.value)
+    if(USERNAME.value && SCORE.value){
+    addItem(USERNAME.value,SCORE.value) //gönderim işlemi
+    alertDOM.innerHTML = alertFunction("Kayıt Başarılı","success")
+    USERNAME.value =""      // gönderimden sonra inputları sıfırla
+    SCORE.value=""
+    }else{
+        alertDOM.innerHTML = alertFunction("Kullanıcı Bilgileri Girilmedi","danger")
+
+    }
+
 }
-
-let userLıstDOM = document.querySelector('userLıst')
-
-const AddItem =(userName, score) =>{
-    let liDOM = document.createElement('li')
-    liDOM.innerHTML(`${userName} ${score}`)
-    userLıstDOM.append(liDOM)
+let userListDOM = document.querySelector('#userList')
+const addItem = (userName,score)=>{
+    let LiDOM = document.createElement('li')
+    LiDOM.innerHTML=`
+        ${userName}
+        <span class="badge bg-primary rounded-pill">${score}</span>
+    `
+    LiDOM.classList.add('list-group-item','d-flex','justify-content-between','align-items-center')
+    userListDOM.append(LiDOM)
 }
-
-
-
-
-
-
-
-
-
-
-
-{/* <li class="list-group-item dflex justify-content-between align-items-center">
-                            asdasdasdas
-    <span class="badge bg-primary rounded-pill"></span>
-</li> */}
