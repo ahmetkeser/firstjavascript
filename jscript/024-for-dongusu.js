9c5eb26e875bab6e522bd801af472df41062a0d7
ahmet keser <ahmetkeser006@gmail.com>
2023-10-08 00:58:10 +0300

    for döngüsü

---
+++ jscript/024-for-dongusu.js
@@ -0,0 +1,17 @@
+let users =["ahmet","mehmet","hasan"]
+// for(let index =0;index<10;index++){
+//     console.log(index)
+// }
+
+// let index =0
+// for(;index<10;index++){
+//     console.log(index)
+// }
+const userListDOM =document.querySelector('#userList')
+let index =0
+for(;index<users.length;index++){
+    const liDOM = document.createElement('li')
+    liDOM.innerHTML=(users[index])
+    userListDOM.appendChild(liDOM)
+}
+
