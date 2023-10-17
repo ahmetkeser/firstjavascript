949fa286b165fb7c7d27a5726af33d8ce5fdef31
ahmetkeser <ahmetkeser006@gmail.com>
2023-10-17 17:43:46 +0300

    Update 038-map.js
    ek bilgi

--- jscript/038-map.js
+++ jscript/038-map.js
@@ -24,3 +24,15 @@ console.log(map1.size);
 // map değer uzunluğu
 console.log(map1.get('c'));
 map1.forEach(element => console.log(element)) // key değerlerini getirir
+
+
+// ekleri düzenle
+const contacts = new Map();
+contacts.set("Jessie", { phone: "213-555-1234", address: "123 N 1st Ave" });
+contacts.has("Jessie"); // true
+contacts.get("Hilary"); // undefined
+contacts.set("Hilary", { phone: "617-555-4321", address: "321 S 2nd St" });
+contacts.get("Jessie"); // {phone: "213-555-1234", address: "123 N 1st Ave"}
+contacts.delete("Raymond"); // false
+contacts.delete("Jessie"); // true
+console.log(contacts.size); // 1
